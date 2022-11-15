d78c3e47e0d92dc47b319479ec67938f6fbd609e
Maxim Hasanov <hasanovmaxim@yandex.ru>
2022-11-15 11:14:01 +0500

    add vuex

---
+++ src/components/popup.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="popup">
+    <select v-model="formData.category">
+      <option v-bind:value="cat" v-for="cat, idx of categories" :key="idx" value="">
+        {{ cat }}
+      </option>
+    </select>
+    <input v-model="formData.count" type="text">
+    <div class="btngroup">
+      <button class="btn-add-el" @click="add">Добавить</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Popup',
+  data() {
+    return {
+      formData: {
+        category: this.categories,
+        count: 0,
+      }
+    }
+  },
+  computed: {
+    categories() {
+      return this.$store.getters.getCategoriesList
+    }
+  },
+  methods: {
+    add() {
+      this.formData.data = new Date().toLocaleDateString('ru-RU', { day: "numeric", month: "numeric", year: "numeric", })
+      this.$store.commit('addPayment', Object.assign({}, this.formData))
+    }
+  },
+  // props: {
+  //   categories: Array,
+  // }
+}
+</script>
+
+<style lang="scss">
+.popup {
+  width: 560px;
+  padding: 30px;
+  background-color: bisque;
+  border: 1px solid black;
+  border-radius: 10px;
+  position: absolute;
+  left: calc(50% - 300px);
+}
+
+.btn-add-el {
+  padding: 10px 0;
+  box-sizing: border-box;
+  margin: 10px 0;
+  width: 125px;
+  border-radius: 7px;
+  border: 1px solid gray;
+}
+</style>
